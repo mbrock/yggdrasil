05dbafffffa503fbd9b219194a32aa85f66b00e1
Mikael Brockman <mikael.brockman@gmail.com>
2013-01-02 19:53:18 +0100

    Add dummy Go WebSockets server

---
+++ src/ygg-broadcast/websocket-broadcast.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"net/http"
+	"flag"
+	"log"
+	"code.google.com/p/go.net/websocket"
+	"github.com/streadway/amqp"
+)
+
+type hub struct {
+	connections map [*connection] bool
+	register chan *connection
+	unregister chan *connection
+	broadcast chan string
+}
+
+var h = hub {
+	register: make(chan *connection),
+	unregister: make(chan *connection),
+	broadcast: make(chan string),
+	connections: make(map[*connection]bool),
+}
+
+func (h *hub) run() {
+	log.Print("Yggdrasil WebSockets Broadcaster running")
+
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = true
+			log.Print("New connection")
+		case c := <-h.unregister:
+			log.Print("Connection closed")
+			delete(h.connections, c)
+			close(c.send)
+		case m := <-h.broadcast:
+			log.Print("Broadcasting message: %v", m)
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+				default:
+					close(c.send)
+					delete(h.connections, c)
+				}
+			}
+		}
+	}
+}
+
+func (h *hub) runAmqpListener() {
+	connection, err := amqp.Dial("amqp://guest:guest@localhost/")
+	if err != nil {
+		log.Fatal("Couldn't dial AMQP server")
+	}
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatal("Couldn't open AMQP channel")
+	}
+	
+	err = channel.ExchangeDeclare(
+		"yggdrasil",
+		"direct",
+		true,
+		false,
+		false,
+		false,
+		nil)
+	if err != nil {
+		log.Fatal("Couldn't open AMQP exchange")
+	}
+
+	state, err := channel.QueueDeclare(
+		"events",
+		true,
+		false,
+		false,
+		false,
+		nil)
+	if err != nil {
+		log.Fatal("Couldn't open AMQP queue")
+	}
+
+	log.Printf("AMQP: %d messages, %d consumers",
+		state.Messages, state.Consumers)
+
+	err = channel.QueueBind(
+		"events",
+		"event",
+		"yggdrasil",
+		false,
+		nil)
+	if err != nil {
+		log.Fatal("Couldn't bind AMQP queue")
+	}
+
+	deliveries, err := channel.Consume(
+		"events",
+		"websocket-broadcast",
+		false,
+		false,
+		false,
+		false,
+		nil)
+	if err != nil {
+		log.Fatal("Couldn't open AMQP consumer")
+	}
+
+	log.Print("Connected to AMQP queue")
+
+	go handle(deliveries)
+}
+	
+func handle(deliveries <-chan amqp.Delivery) {
+	for d := range deliveries {
+		log.Printf("got %dB: [%v] %s",
+			len(d.Body),
+			d.DeliveryTag,
+			d.Body)
+	}
+}
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func main() {
+	flag.Parse()
+	go h.run()
+	go h.runAmqpListener()
+	http.Handle("/", websocket.Handler(wsHandler))
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
